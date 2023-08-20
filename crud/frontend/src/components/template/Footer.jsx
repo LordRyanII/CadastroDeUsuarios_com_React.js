d4dc4421cc15be00b6ce015244f3794aa9de22a3
LordRyanII <ryanoliveiram2020@outlook.com>
2023-08-20 17:43:17 -0300

    Initial commit

---
+++ crud/frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido Com <i className="fa fa-heart text-danger"></i> por
+            <strong> Ryan Menezes - Dev FullStack</strong>
+        </span>
+    </footer>
