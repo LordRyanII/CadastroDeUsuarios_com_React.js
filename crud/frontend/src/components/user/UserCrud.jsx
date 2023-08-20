2ab444818b167aad70dcd6d71dcc2afdc3f7dd88
LordRyanII <ryanoliveiram2020@outlook.com>
2023-08-20 18:23:19 -0300

    Finalizando projeto

--- crud/frontend/src/components/user/UserCrud.jsx
+++ crud/frontend/src/components/user/UserCrud.jsx
@@ -18,8 +18,9 @@ export default class UserCrud extends Component {
   state = { ...initialState };
  
   componentWillMount() {
-    axios(baseUrl).then(resp => {
-      this.setState({ list: resp.data });
+    axios(baseUrl)
+        .then(resp => {
+            this.setState({ list: resp.data });
     });
   }
  
@@ -31,9 +32,10 @@ export default class UserCrud extends Component {
     const user = this.state.user;
     const method = user.id ? "put" : "post";
     const url = user.id ? `${baseUrl}/${user.id}` : baseUrl;
-    axios[method](url, user).then(resp => {
-      const list = this.getUpdatedList(resp.data);
-      this.setState({ user: initialState.user, list });
+    axios[method](url, user)
+        .then(resp => {
+            const list = this.getUpdatedList(resp.data);
+            this.setState({ user: initialState.user, list });
     });
   }
  
@@ -103,9 +105,10 @@ export default class UserCrud extends Component {
   }
  
   remove(user) {
-    axios.delete(`${baseUrl}/${user.id}`).then(resp => {
-      const list = this.getUpdatedList(user, false);
-      this.setState({ list });
+    axios.delete(`${baseUrl}/${user.id}`)
+        .then(resp => {
+            const list = this.getUpdatedList(user, false);
+            this.setState({ list });
     });
   }
  
